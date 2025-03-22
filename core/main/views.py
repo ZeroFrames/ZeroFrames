4fca1186439953794192fcc5ec3e3d9fd70802fb
Wizer27 <svd2yar@gmail.com>
2025-03-22 19:13:36 +0100

    add html and css

--- core/main/views.py
+++ core/main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return render(request,'main/1.html')
-    
+
 def about(request):
-    return HttpResponse("<h4>ZeroFrames is an easy-to-use tool for creating custom graphics for your Flipper Zero. You can design icons, frames, and other visuals to personalize your device. The tool lets you preview your designs in real-time, export them easily, and even share them with others.</h4>")
+    return render(request,'main/2.html')
+    
     
