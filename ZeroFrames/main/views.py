1e34122c0b0379d7616443ca12ff852c3cbef33e
Wizer27 <svd2yar@gmail.com>
2025-03-22 15:03:12 +0100

    add animation

--- ZeroFrames/main/views.py
+++ ZeroFrames/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("<h4>Test test welcome welcome</h4>")
+    return render(request,'main/1.html')
+    
 def about(request):
     return HttpResponse("<h4>ZeroFrames is an easy-to-use tool for creating custom graphics for your Flipper Zero. You can design icons, frames, and other visuals to personalize your device. The tool lets you preview your designs in real-time, export them easily, and even share them with others.</h4>")
-
+    
